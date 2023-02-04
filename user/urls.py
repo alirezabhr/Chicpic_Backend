468fa4fb856f6ba979e66c7cc7b378755de9edb7
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-02-04 16:11:41 +0330

    Add otp request and verify urls.

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.SignupView.as_view(), name='signup'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('otp/request/', views.RequestOTPView.as_view(), name='request_otp'),
+    path('otp/verify/', views.VerifyOTPView.as_view(), name='verify_otp'),
 ]
