3bc59b257683d4f356f683f45fa37f20a3b94f0e
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-02-04 16:11:20 +0330

    Create custom throttle and OTP views.
     - Create OncePerMinuteThrottle to use in otp request
     - Implement RequestOTPView and VerifyOTPView to create and check otp for users.

--- user/views.py
+++ user/views.py
@@ -2,8 +2,16 @@ from rest_framework.generics import CreateAPIView, GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.throttling import UserRateThrottle
 
-from .serializers import UserRegistrationSerializer, UserLoginSerializer
+from django.contrib.auth import get_user_model
+
+from .serializers import UserRegistrationSerializer, UserLoginSerializer, OTPRequestSerializer, OTPVerifySerializer
+
+
+class OncePerMinuteThrottle(UserRateThrottle):
+    rate = '1/minute'
 
 
 class SignupView(CreateAPIView):
@@ -20,3 +28,28 @@ class LoginView(GenericAPIView):
         if ser.is_valid():
             return Response(ser.data, status=status.HTTP_200_OK)
         return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RequestOTPView(APIView):
+    throttle_classes = (OncePerMinuteThrottle,)
+
+    def post(self, request):
+        ser = OTPRequestSerializer(data=request.data)
+
+        if ser.is_valid():
+            ser.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class VerifyOTPView(APIView):
+    def post(self, request):
+        ser = OTPVerifySerializer(data=request.data)
+        if ser.is_valid():
+            user = get_user_model().objects.get(id=ser.data.get('user_id'))
+            user.is_verified = True
+            user.save()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
