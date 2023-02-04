d08b6fbdc2279bcda27cbd49864a04211b0a9621
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-02-04 10:36:05 +0330

    Create User model and set it in Auth User Model in settings.

--- chicpic/settings.py
+++ chicpic/settings.py
@@ -179,3 +179,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# User model
+AUTH_USER_MODEL = 'user.User'
+
+AUTHENTICATION_BACKENDS = [
+    'user.backends.EmailUsernameAuthenticationBackend'
+]
--- user/models.py
+++ user/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .managers import CustomUserManager
+from .validators import CustomUsernameValidator
+
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    username_validator = CustomUsernameValidator()
+
+    email = models.EmailField(max_length=60, unique=True)
+    username = models.CharField(max_length=30, unique=True, validators=[username_validator])
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    groups = None
+    user_permissions = None
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def tokens(self):
+        tokens = RefreshToken.for_user(self)
+        return {
+            'access': str(tokens.access_token),
+            'refresh': str(tokens),
+        }
+
+    def __str__(self):
+        return self.username
