3b9bf32b70cf010b778b123fe90500e3e8a17125
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-04-04 22:19:46 +0330

    Add a function to find proper and similar choice in choice fields.

--- scraper/utils.py
+++ scraper/utils.py
@@ -4,3 +4,18 @@ import re
 def remove_html_tags(html_data: str):
     clean = re.compile('<.*?>')  # compile the regular expression pattern to match HTML tags
     return re.sub(clean, '', html_data)  # remove the matched tags from the input string
+
+
+def find_proper_choice(choices: list, key: str) -> str:
+    choice_value = ''
+
+    # choices should be a list of tuples contain database value and human readable text
+    for choice_db_value, human_readable_text in choices:
+        if (choice_db_value.lower() in key.lower()) or \
+                (key.lower() in choice_db_value.lower()) or \
+                (key.lower() in human_readable_text.lower()) or \
+                (human_readable_text.lower() in key.lower()):
+
+            choice_value = choice_db_value
+
+    return choice_value
