9c0de47350434cdbfabd5985340779057027760c
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-04-08 13:40:16 +0330

    Register models in admin panel.

--- clothing/admin.py
+++ clothing/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from clothing.models import Category, Shop, Product, Variant, Attribute, SizeGuide, SavedVariant, TrackedVariant
+
+admin.site.register(Category)
+admin.site.register(Shop)
+admin.site.register(Product)
+admin.site.register(Variant)
+admin.site.register(Attribute)
+admin.site.register(SizeGuide)
+admin.site.register(TrackedVariant)
+admin.site.register(SavedVariant)
