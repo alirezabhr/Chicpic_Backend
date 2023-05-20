750b36b4d893426997c03fbc5046139941c0f4b6
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-05-20 08:49:07 +0330

    Add Rest Framework config to avoid coerce decimals to string.

--- chicpic/settings.py
+++ chicpic/settings.py
@@ -106,6 +106,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 24,
+    'COERCE_DECIMAL_TO_STRING': False,
 }
 
 # Database
