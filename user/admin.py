2da985a78579e763e0ad32b1ae05e16e467d12ef
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-10-05 13:00:48 +0330

    Register models in admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from user.models import User, UserAdditional, ShirtFit, TrouserFit
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(UserAdditional)
+admin.site.register(ShirtFit)
+admin.site.register(TrouserFit)
