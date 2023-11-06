472f67967698a3899950371395e92a7035d83aa3
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-11-06 18:58:40 +0330

    Add user additional in login and signup serializers.

--- user/serializers.py
+++ user/serializers.py
@@ -9,72 +9,6 @@ from .models import OTP, UserAdditional, ShirtFit, TrouserFit
 User = get_user_model()
 
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(min_length=8, write_only=True, required=True)
-    password2 = serializers.CharField(min_length=8, write_only=True, required=True)
-
-    class Meta:
-        model = User
-        fields = ('id', 'email', 'username', 'is_verified', 'password', 'password2', 'tokens')
-        read_only_fields = ('id', 'tokens')
-
-    def validate(self, attrs):
-        if attrs.get('password') != attrs.get('password2'):
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-
-        return attrs
-
-    def validate_username(self, username):
-        if len(username) < 2:
-            raise serializers.ValidationError('Username must contain at least 2 characters.')
-        if self.Meta.model.objects.filter(username__iexact=username).exists():
-            raise serializers.ValidationError('User with this username already exists.')
-
-        return username
-
-    def validate_email(self, email):
-        if self.Meta.model.objects.filter(email__iexact=self.Meta.model.objects.normalize_email(email)).exists():
-            raise serializers.ValidationError('User with this email already exists.')
-
-        return email
-
-    def create(self, validated_data):
-        user = self.Meta.model.objects.create_user(
-            email=validated_data['email'],
-            username=validated_data['username'],
-            password=validated_data['password'],
-        )
-        return user
-
-
-class UserLoginSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(max_length=30)
-    password = serializers.CharField(write_only=True, required=True)
-
-    class Meta:
-        model = User
-        fields = ('id', 'email', 'username', 'is_verified', 'password', 'tokens')
-        read_only_fields = ('id', 'email', 'tokens')
-
-    def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
-
-        user = authenticate(username=username, password=password)
-
-        if not user:
-            raise AuthenticationFailed('Invalid credentials.')  # It will return 403
-        if not user.is_verified:
-            raise serializers.ValidationError({'email': 'Your email is not verified.'})
-
-        attrs['id'] = user.id
-        attrs['username'] = user.username
-        attrs['email'] = user.email
-        attrs['is_verified'] = user.is_verified
-        attrs['tokens'] = user.tokens()
-        return attrs
-
-
 class ShirtFitSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShirtFit
@@ -127,6 +61,74 @@ class UserAdditionalSerializer(serializers.ModelSerializer):
             TrouserFit.objects.create(user_additional=user_additional, **trouser_fit_data)
 
         return user_additional
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=8, write_only=True, required=True)
+    password2 = serializers.CharField(min_length=8, write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'is_verified', 'password', 'password2', 'tokens', 'additional')
+        read_only_fields = ('id', 'tokens', 'additional')
+
+    def validate(self, attrs):
+        if attrs.get('password') != attrs.get('password2'):
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def validate_username(self, username):
+        if len(username) < 2:
+            raise serializers.ValidationError('Username must contain at least 2 characters.')
+        if self.Meta.model.objects.filter(username__iexact=username).exists():
+            raise serializers.ValidationError('User with this username already exists.')
+
+        return username
+
+    def validate_email(self, email):
+        if self.Meta.model.objects.filter(email__iexact=self.Meta.model.objects.normalize_email(email)).exists():
+            raise serializers.ValidationError('User with this email already exists.')
+
+        return email
+
+    def create(self, validated_data):
+        user = self.Meta.model.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+        return user
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=30)
+    password = serializers.CharField(write_only=True, required=True)
+    additional = UserAdditionalSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'is_verified', 'password', 'tokens', 'additional')
+        read_only_fields = ('id', 'email', 'tokens', 'additional')
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            raise AuthenticationFailed('Invalid credentials.')  # It will return 403
+        if not user.is_verified:
+            raise serializers.ValidationError({'email': 'Your email is not verified.'})
+
+        attrs['id'] = user.id
+        attrs['username'] = user.username
+        attrs['email'] = user.email
+        attrs['is_verified'] = user.is_verified
+        attrs['tokens'] = user.tokens()
+        attrs['additional'] = user.additional
+        return attrs
 
 
 class UserReadonlySerializer(serializers.ModelSerializer):
